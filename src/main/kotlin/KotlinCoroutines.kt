5aba01de1c20abb36c1be66e059a0abe3d2dc22e
Matheus Bosa <matheusbozza@gmail.com>
2022-07-09 20:22:49 -0300

    refactor: :art: Using custom suspend function

--- src/main/kotlin/KotlinCoroutines.kt
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -8,10 +8,14 @@ fun main() = runBlocking { // Creates a coroutine that blocks the current main t
 
     GlobalScope.launch { // Thread: T1
         println("Fake work starts: ${Thread.currentThread().name}") // Thread: T1
-        delay(1000) // Coroutine is suspended but Thread: T1 is free (not blocked)
+        wait(1000) // Coroutine is suspended but Thread: T1 is free (not blocked)
         println("Fake work finished: ${Thread.currentThread().name}") // Either T1 or some other thread
     }
 
-    delay(2000) // Main thread: wait for coroutine to finish
+    wait(2000) // Main thread: wait for coroutine to finish
     println("Main program ends: ${Thread.currentThread().name}") // Main thread
+}
+
+private suspend fun wait(millis: Long) {
+    delay(millis)
 }
